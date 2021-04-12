8ad2f04135a690993bbb0352bad9bd7a391e2c17
Prohor Sevastianov <sevastyanov.p@oneway.su>
2021-04-12 21:57:02 +0300

    lesson6

--- src/components/chat-list/chat-list.js
+++ src/components/chat-list/chat-list.js
@@ -2,55 +2,62 @@ import { List, Button } from "@material-ui/core"
 import React, { Component } from "react"
 import { connect } from "react-redux"
 import { Link } from "react-router-dom"
-import { AddContactModal } from "../modal"
+import { AddContactModal } from "../add-contact-modal"
 import { Chat } from "./chat"
 
 export class ChatListView extends Component {
-  state = {
-    isOpen: false,
-  }
+    state = {
+        isOpen: false,
+    }
 
-  toggleModal = () => {
-    this.setState({
-      isOpen: !this.state.isOpen,
-    })
-  }
+    toggleModal = () => {
+        this.setState({
+            isOpen: !this.state.isOpen,
+        })
+    }
 
-  render() {
-    const { conversations, match } = this.props
-    const { isOpen } = this.state
-    const { id } = match?.params
+    render() {
+        const { conversations, match, messages } = this.props
+        const { isOpen } = this.state
+        const { id } = match.params
 
-    return (
-        <>
-          <div>
-            <List component="nav">
-              {conversations.map((chat) => {
-                return (
-                    <Link key={chat.title} to={`/chat/${chat.title}`}>
-                      <Chat selected={chat.title === id} chat={chat} />
-                    </Link>
-                )
-              })}
-            </List>
-          </div>
+        return (
+            <>
+                <div>
+                    <List component="nav">
+                        {conversations.map((chat) => {
+                            const msg = messages[chat.title] || []
 
-          <AddContactModal
-              conversations={conversations}
-              isOpen={isOpen}
-              onClose={this.toggleModal}
-          />
+                            return (
+                                <Link key={chat.title} to={`/chat/${chat.title}`}>
+                                    <Chat
+                                        selected={chat.title === id}
+                                        chat={chat}
+                                        lastMessage={msg[msg.length - 1]}
+                                    />
+                                </Link>
+                            )
+                        })}
+                    </List>
+                </div>
 
-          <Button variant="contained" fullWidth={true} onClick={this.toggleModal}>
-            Добавить чат
-          </Button>
-        </>
-    )
-  }
+                <AddContactModal
+                    isOpen={isOpen}
+                    onClose={this.toggleModal}
+                    conversations={conversations}
+                />
+
+                <Button variant="contained" fullWidth={true} onClick={this.toggleModal}>
+                    Добавить чат
+                </Button>
+            </>
+        )
+    }
 }
 
 const mapStateToProps = (state) => ({
-  conversations: state.conversationsReducer,
+    conversations: state.conversationsReducer,
+    messages: state.messagesReducer,
 })
 
 export const ChatList = connect(mapStateToProps, null)(ChatListView)
--- src/store/messages/actions.js
+++ src/store/messages/actions.js
@@ -1,9 +1,8 @@
-import { MESSAGE_SEND } from "./types";
+import { MESSAGE_SEND } from "./types"
 
-// params {author, message, roomId}
 export const sendMessage = (params) => {
   return {
     type: MESSAGE_SEND,
     payload: params,
-  };
-};
+  }
+}
